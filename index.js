a7c83110820421026997b998d86ecd0ab4f251ec
Csaba Koncz <ckoncz@cloudera.com>
2021-12-22 20:22:57 +0100

    Github login support for ftg

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const ParseDashboard = require('parse-dashboard');
 const {crypt_check, crypt_check2} = require('./firebase-auth')
 const  cors = require('cors')
 const bp = require('body-parser')
+const axios = require('axios')
 
 const PORT = process.env.PORT || 5000
 
 const PARSE_MASTER_KEY = process.env.PARSE_MASTER_KEY
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 
 if (!PARSE_MASTER_KEY){
   console.log('PARSE_MASTER_KEY must defined as an environment variable!')
@@ -55,6 +57,32 @@ const parseDashboard = new ParseDashboard({
   useEncryptedPasswords: false
 })
 
+async function github_auth_cb(req,res){
+
+  // this is the request that should bring us here:
+  // https://github.com/login/oauth/authorize?client_id=2424e4e1c3bbe5132a2c&redirect_uri=https://csabak.herokuapp.com/ftg-github-auth/v4/&scope=repo
+
+  let response = await axios.post('https://github.com/login/oauth/access_token',{
+    client_id: '2424e4e1c3bbe5132a2c',
+    client_secret: GITHUB_CLIENT_SECRET,
+    code: req.query.code
+    },
+    {
+      headers:{
+        'accept': 'application/json'
+      }
+    })
+    if(response.data.access_token){
+      let token_type = response.data.token_type
+      let access_token=response.data.access_token
+      let token_param=`t=${token_type} ${access_token}`
+      res.redirect(301,`https://csabakoncz.github.io/ftg/${req.params.version}#${token_param}`)
+    }
+    else{
+      res.send(response.data)
+    }
+}
+
 express()
   // .options('*', cors())
   .use(cors())
@@ -64,6 +92,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/ftg-github-auth/:version',github_auth_cb)
   .get('/cool', (req, res) => res.send(cool()))
   .post('/crypt_check', bp.json(), crypt_check)
   .post('/crypt_check2', bp.json(), crypt_check2)
